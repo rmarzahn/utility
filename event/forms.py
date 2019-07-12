494be04dca1f25c7733616ebf1f8211d8e255958
rebecca <rebecca.marzahn@outlook.com>
2019-07-12 19:41:44 +0200

    utility_v12

--- event/forms.py
+++ event/forms.py
@@ -8,8 +8,8 @@ class TimeInput(forms.TimeInput):
     input_type = 'time'
 
 class CreateEvent(forms.ModelForm):
-    date = forms.DateField(widget=DateInput)
-    time = forms.TimeField(widget=TimeInput)
+    date = forms.DateField(help_text = 'Format: tt.mm.jjjj', widget=DateInput)
+    time = forms.TimeField(help_text = 'Format: --:--', widget=TimeInput)
     class Meta:
         model = models.Event
         fields = ['title', 'text', 'date', 'time', 'street_number', 'postalcode', 'city']
--- event/models.py
+++ event/models.py
@@ -7,11 +7,8 @@ from datetime import date
 class Event(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField()
-    date = models.DateField(auto_now = False, auto_now_add = False, default=timezone.now)
-    time = models.TimeField(auto_now = False, auto_now_add = False, default=timezone.now)
+    date = models.DateField(auto_now = False, auto_now_add = False, default=timezone.now, help_text = 'Format: tt.mm.jjjj')
+    time = models.TimeField(auto_now = False, auto_now_add = False, default=timezone.now, help_text = 'Format: --:--')
     street_number = models.CharField(max_length=50)
     postalcode = models.CharField(max_length=5)
     city = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.title
