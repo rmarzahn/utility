d73fc38bcdb8fead59e99137bb7d0f11a9376834
rebecca <rebecca.marzahn@outlook.com>
2019-07-15 12:15:30 +0200

    utility_v12

--- event/forms.py
+++ event/forms.py
@@ -8,8 +8,8 @@ class TimeInput(forms.TimeInput):
     input_type = 'time'
 
 class CreateEvent(forms.ModelForm):
-    date = forms.DateField(help_text = 'Format: tt.mm.jjjj', widget=DateInput)
-    time = forms.TimeField(help_text = 'Format: --:--', widget=TimeInput)
+    date = forms.DateField(widget=DateInput, label = 'Date (Format: tt.mm.jjjj)')
+    time = forms.TimeField(widget=TimeInput, label = 'Time (Format: --:--)')
     class Meta:
         model = models.Event
         fields = ['title', 'text', 'date', 'time', 'street_number', 'postalcode', 'city']
--- event/models.py
+++ event/models.py
@@ -7,8 +7,9 @@ from datetime import date
 class Event(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField()
-    date = models.DateField(auto_now = False, auto_now_add = False, default=timezone.now, help_text = 'Format: tt.mm.jjjj')
-    time = models.TimeField(auto_now = False, auto_now_add = False, default=timezone.now, help_text = 'Format: --:--')
-    street_number = models.CharField(max_length=50)
+    date = models.DateField(auto_now = False, auto_now_add = False, default=timezone.now, verbose_name = 'Date Format (tt.mm.jjjj)')
+    time = models.TimeField(auto_now = False, auto_now_add = False, default=timezone.now, verbose_name = 'Time Format (--:--)')
+    street_number = models.CharField(max_length=50, verbose_name = 'Street and Number')
     postalcode = models.CharField(max_length=5)
     city = models.CharField(max_length=50)
+
